1f16ddad3a88f0323c32c28e7347d8977f5329db
Anurag Devanapally <anurag.devanapally@gmail.com>
2026-03-24 22:58:43 -0700

    Remove no-op ZSH_CUSTOM re-export

--- zsh/zsh-custom/10-flags.zsh
+++ zsh/zsh-custom/10-flags.zsh
@@ -1,9 +1,6 @@
 export LDFLAGS="-L${BREW_PREFIX}/opt/ruby/lib"
 export CPPFLAGS="-I${BREW_PREFIX}/opt/ruby/include"
 
-# Export $ZSH_CUSTOM explicitly for access in bash scripts.
-export ZSH_CUSTOM="$ZSH_CUSTOM"
-
 # Avoid opening sub-editors (default oh-my-zsh behavior) for certain commands.
 export PAGER='less'
 export LESS='-R'
