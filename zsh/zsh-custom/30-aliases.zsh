7c9b17dd490815191fb9f5a78dbef8ca9844971e
Anurag Devanapally <anurag.devanapally@gmail.com>
2026-03-25 00:03:58 -0700

    Add my most-used Flutter aliases

--- zsh/zsh-custom/30-aliases.zsh
+++ zsh/zsh-custom/30-aliases.zsh
@@ -6,6 +6,18 @@ alias lsa="ls -AFGho"
 alias mdlint='markdownlint-cli2 --config ~/.markdownlint.yml'
 alias pip='python3 -m pip'
 
+flu() {
+    case "$1" in
+        a)   flutter analyze "${@:2}" ;;
+        b)   flutter build "${@:2}" ;;
+        c)   flutter clean "${@:2}" ;;
+        doc) flutter doctor "${@:2}" ;;
+        pg)  flutter pub get ;;
+        t)   flutter test "${@:2}" ;;
+        *)   flutter "$@" ;;
+    esac
+}
+
 allansicolors() {
     for i in {0..255}; do
         printf '\033[38;5;%dm████ %3d\033[0m\n' $i $i
